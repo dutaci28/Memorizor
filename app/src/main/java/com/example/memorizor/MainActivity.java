704f716647fca1470e6c9f784d41e39ce36968b1
Catalin Duta <dutaci28@gmail.com>
2022-03-19 21:45:38 +0200

    implemented menu bar

--- app/src/main/java/com/example/memorizor/MainActivity.java
+++ app/src/main/java/com/example/memorizor/MainActivity.java
@@ -1,10 +1,23 @@
 package com.example.memorizor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.memorizor.Fragments.AccountFragment;
+import com.example.memorizor.Fragments.BookmarksFragment;
+import com.example.memorizor.Fragments.HomeFragment;
+import com.example.memorizor.Fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private Fragment selectorFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +26,43 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+                    case R.id.nav_home :
+                        selectorFragment = new HomeFragment();
+                        break;
+
+                    case R.id.nav_search :
+                        selectorFragment = new SearchFragment();
+                        break;
+
+//                    case R.id.nav_add :
+//                        selectorFragment = null;
+//                        startActivity(new Intent(MainActivity.this , PostActivity.class));
+//                        break;
+
+                    case R.id.nav_bookmarks:
+                        selectorFragment = new BookmarksFragment();
+                        break;
+
+                    case R.id.nav_account:
+                        selectorFragment = new AccountFragment();
+                        break;
+                }
+
+                if (selectorFragment != null){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container , selectorFragment).commit();
+                }
+
+                return  true;
+
+            }
+        });
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container , new HomeFragment()).commit();
     }
 }
