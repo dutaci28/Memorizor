99788668c08ad31a477e0e59522cdcd74cc4627f
Catalin Duta <dutaci28@gmail.com>
2022-06-12 14:14:20 +0300

    video player revamp

--- app/src/main/java/com/example/memorizor/VideoActivity.java
+++ app/src/main/java/com/example/memorizor/VideoActivity.java
@@ -3,13 +3,39 @@ package com.example.memorizor;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.MediaController;
+import android.widget.ProgressBar;
 import android.widget.VideoView;
 
 import com.example.memorizor.Model.Video;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -18,10 +44,14 @@ import com.google.firebase.database.ValueEventListener;
 
 public class VideoActivity extends AppCompatActivity {
 
+    PlayerView playerView;
+    ProgressBar progressBar;
+    ImageView btFullScreen;
+    SimpleExoPlayer simpleExoPlayer;
+    boolean flag = false;
+
     private String videoId;
     private Video currentVideo;
-
-    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +59,13 @@ public class VideoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_video);
         getSupportActionBar().hide();
 
+        playerView = findViewById(R.id.player_view);
+        progressBar = findViewById(R.id.progress_bar);
+        btFullScreen = findViewById(R.id.bt_fullscreen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         videoId = getIntent().getStringExtra("videoId");
-        videoView = findViewById(R.id.video_view);
 
         Query query = FirebaseDatabase.getInstance().getReference().child("Videos")
                 .orderByChild("videoId").startAt(videoId).endAt(videoId + "\uf8ff");
@@ -43,18 +78,47 @@ public class VideoActivity extends AppCompatActivity {
                     currentVideo = video;
                 }
 
-                MediaController mediaController = new MediaController(VideoActivity.this);
-                mediaController.setAnchorView(videoView);
-                videoView.setMediaController(mediaController);
-                videoView.setVideoPath(currentVideo.getVideoUrl());
-                videoView.requestFocus();
-                videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                Uri videoUrl = Uri.parse(currentVideo.getVideoUrl());
+
+                LoadControl loadControl = new DefaultLoadControl();
+                BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+                TrackSelector trackSelector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(bandwidthMeter));
+                simpleExoPlayer = ExoPlayerFactory.newSimpleInstance(VideoActivity.this,trackSelector,loadControl);
+                DefaultHttpDataSourceFactory factory = new DefaultHttpDataSourceFactory("exoplayer_video");
+                ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+                MediaSource mediaSource = new ExtractorMediaSource(videoUrl,factory,extractorsFactory,null,null);
+                playerView.setPlayer(simpleExoPlayer);
+                playerView.setKeepScreenOn(true);
+                simpleExoPlayer.prepare(mediaSource);
+                simpleExoPlayer.setPlayWhenReady(true);
+                simpleExoPlayer.addListener(new Player.DefaultEventListener() {
+
                     @Override
-                    public void onPrepared(MediaPlayer mp) {
-                        videoView.pause();
+                    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                        super.onPlayerStateChanged(playWhenReady, playbackState);
+                        if(playbackState == Player.STATE_BUFFERING){
+                            progressBar.setVisibility(View.VISIBLE);
+                        } else if (playbackState == Player.STATE_READY){
+                            progressBar.setVisibility(View.GONE);
+                        }
                     }
+
                 });
 
+                btFullScreen.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(flag){
+                            btFullScreen.setImageDrawable(getResources().getDrawable(R.drawable.ic_fullscreen));
+                            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                            flag=false;
+                        } else {
+                            btFullScreen.setImageDrawable(getResources().getDrawable(R.drawable.ic_fullscreen_exit));
+                            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                            flag=true;
+                        }
+                    }
+                });
             }
 
             @Override
@@ -62,8 +126,18 @@ public class VideoActivity extends AppCompatActivity {
 
             }
         });
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        simpleExoPlayer.setPlayWhenReady(false);
+        simpleExoPlayer.getPlaybackState();
+    }
 
-
-
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        simpleExoPlayer.setPlayWhenReady(true);
+        simpleExoPlayer.getPlaybackState();
     }
 }
